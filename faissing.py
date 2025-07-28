73d02c270b831997055dc575b0ce94a129693510
Sakshamsetia <setiasaksham15@gmail.com>
2025-07-29 00:24:47 +0530

    faiss and custom model codes

---
+++ faissing.py
@@ -0,0 +1,15 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+
+
+def faiss():
+    pdf = PyPDFLoader('./PDFs/test.pdf')
+    data = pdf.load()
+    model = HuggingFaceEmbeddings('all-MiniLM-L6-v2')
+    
+    vectorDb = FAISS().from_documents(documents=data,embedding=model)
+    return vectorDb
+
+if __name__=="__main__":
+    print("Hello World")
---
+++ model.py
@@ -0,0 +1,49 @@
+from langchain_core.language_models import BaseChatModel
+import google.genai as genai
+from langchain_core.messages import AIMessage
+from langchain_core.prompt_values import StringPromptValue
+from dotenv import load_dotenv
+import os
+from google.genai import types
+
+class ChatModel(BaseChatModel):
+    def __init__(self):
+        load_dotenv(override=True)
+        api_key = os.getenv("GEMINI_API_KEY")
+        client = genai.Client(api_key=api_key)
+        object.__setattr__(self,client,"client")
+        sysPrompt = types.GenerateContentConfig(
+            temperature=0.45,
+            system_prompt = """
+First try to answer based on the uploaded PDF.
+If no relevant information is found, you may use general knowledge to help.
+Be transparent if you're going beyond the document.
+"""
+        )
+        object.__setattr__(self,sysPrompt,"sysPrompt")
+        
+    
+    def _generate(self, messages:str)->str:
+        response = self.client.models.generate_content(
+            contents=messages,
+            model='gemini-2.0-flash',
+            config=self.sysPrompt
+        )
+        return response.text
+    
+    def invoke(self, input:StringPromptValue, config = None)->AIMessage:
+        msg = input.to_string()
+        response = self._generate(msg)
+        return AIMessage(content=response)
+                
+    @property
+    def streaming(self):
+        return False
+    
+    @property
+    def _llm_type(self):
+        return "Custom_gemini"
+    
+    @property
+    def callback(self):
+        return []
